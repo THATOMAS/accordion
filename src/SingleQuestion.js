6caf226b82d2ba19150536cd881fb76485454dd6
THATOMAS <chachumashori@gmail.com>
2021-11-22 07:39:30 +0200

    i removed all state values at the top of the function,and replaced them with one state value.The const [showInfo,setShowInfo] = useState(false).
    And then i removed all the code that referenced the previous state values and replaced
    the onclick with a changing show info function,wheres when i click on the button the
    state for show info changes between true and false.In the button tag i gave it
    a Ternary operator wheres,if showinfo is true show the minus icon eg ... {showInfo ? <icon1/>:<icon2/>}
     if not show the other one .When its true,
    i gave the <p> tag a {showInfo && info} and operator .

--- src/SingleQuestion.js
+++ src/SingleQuestion.js
@@ -6,26 +6,14 @@ import {AiFillPlusCircle,AiFillMinusCircle} from "react-icons/ai"
 
 
 const Info = ({id,info,title})=>{
-    const [index,setIndex] = useState(0)
-    const [toggle,setToggle] = useState(false)
-    const icons = [<AiFillPlusCircle/>,<AiFillMinusCircle/>] 
-    const changeIcon = ()=>{
-        if (index === 0){
-            setIndex(1)
-            setToggle(!toggle)
-        }
-        if (index === 1){
-            setIndex(0)
-            setToggle(false)
-        }
-    }
+    const [showInfo,setShowInfo] = useState(false)
     
 
 
 return <div className="titlebox">
 <h4>{title}</h4>
-<button onClick={changeIcon}>{icons[index]}</button>
-<p>{toggle ? info:<></>}</p>
+<button onClick={()=>setShowInfo(!showInfo)}>{showInfo? <AiFillMinusCircle/> : <AiFillPlusCircle/>}</button>
+<p>{showInfo && info}</p>
 </div>
 }
 
